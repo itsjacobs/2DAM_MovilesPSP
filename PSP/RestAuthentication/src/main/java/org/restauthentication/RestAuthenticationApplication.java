baa1e62b255ffa037fbd7ab1554d67c63aa07261
Jacob Rosales <jakovrosales@gmail.com>
2025-11-15 20:41:21 +0100

    Update

---
+++ PSP/RestAuthentication/src/main/java/org/restauthentication/RestAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package org.restauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestAuthenticationApplication.class, args);
+    }
+
+}
